4c057bdae5360dbb2ea23c9446e0fdf7c3a90bef
Brian Jahnke <bjahnke71@gmail.com>
2024-04-16 16:31:02 -0400

    Remove duplicate listener, translate new project name to slug to handle spaces in id

--- src/index.js
+++ src/index.js
@@ -10,42 +10,20 @@ document.querySelector('#new-task').addEventListener('click', () => {
 document.querySelector('#add-project').addEventListener('click', (event) => {
   event.preventDefault()
   const projectInput = document.querySelector('#project-name-input')
-  const projectName = projectInput.value
-  if (projectName === '') {
+  const projectName = projectInput.value.trim()
+  const projectSlug = projectName.toLowerCase().replace(' ', '-')
+  if (projectSlug === '') {
     return
   }
-  if (document.querySelector(`#${projectName}`)) {
+  if (document.querySelector(`#${projectSlug}`)) {
     alert(`Project ${projectName} already exists! Please enter a different name.`)
     return
   }
   const project = document.createElement('button')
   project.textContent = projectName
-  project.id = projectName
+  project.id = projectSlug
   project.className = 'project-button'
   document.querySelector('#project-list').append(project)
   projectInput.value = ''
   projectInput.focus()
-})
-
-document.querySelector('#projects').addEventListener('click', (event) => {
-  event.preventDefault()
-  const projectInput = document.querySelector('#project-name-input')
-  const projectName = projectInput.value
-  if (projectName === '') {
-    return
-  }
-  if (document.querySelector(`#${projectName}`)) {
-    alert(`Project ${projectName} already exists! Please enter a different name.`)
-    return
-  }
-  const project = document.createElement('button')
-  project.textContent = projectName
-  project.id = projectName
-  project.className = 'project-button'
-  document.querySelector('#project-list').append(project)
-  projectInput.value = ''
-  projectInput.focus()
-
-  // Scroll to the bottom of the project list
-  window.scrollTo(0, document.querySelector('#project-list').scrollHeight)
 })
